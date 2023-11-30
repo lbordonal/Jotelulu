1f9a32086c470eee72e1c90b73eed9a76847b22b
Lucas Bordonal <lbordonal@gmail.com>
2023-11-30 14:27:18 +0000

    30/11/23 - 14:27

---
+++ S3 - Ubuntu configuration/backup_s3.sh
@@ -0,0 +1,1 @@
+aws --profile=3D"backup_lucas" --endpoint-url=https://s3.jotelulu.com s3 sync /home/lbordonal/Downloads/teste_s3/. s3://dep000-repositrios31/Teste_Lucas
---
+++ S3 - Ubuntu configuration/create.sh
@@ -0,0 +1,37 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    create.sh                                          :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: lbordona <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2023/11/30 14:02:48 by lbordona          #+#    #+#              #
+#    Updated: 2023/11/30 14:25:51 by lbordona         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+echo	"URL: <Ver Portal Jotelulu>"
+read	link
+
+echo	"Bucket: <Ver Portal Jotelulu>"
+read	bucket
+
+echo	"Access Key: <Ver Portal Jotelulu no campo Utilizadores>"
+read	access
+
+echo	"Secret Key: <Ver Portal Jotelulu no campo Utilizadores>"
+read	secret
+
+echo	"Nome do Perfil: <Definido na configuração da CLI>"
+read	profile
+
+echo	"Caminho da Pasta Local: <Pasta que deseja fazer o backup>"
+read	local
+
+echo	"Caminho da Pasta Remota: <Pasta no Repositório S3 da Jotelulu>"
+read	remote
+
+echo	"aws --profile=3D\"$profile\" --endpoint-url=$link s3 sync "$local/." s3://$bucket/$remote" > backup.sh
+
+chmod +x backup.sh
+
